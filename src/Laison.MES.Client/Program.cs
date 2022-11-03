baa89b109edb783f5bffd96922518e1f64167cb3
fjq <180489097@qq.com>
2022-11-03 14:25:09 +0800

    update

--- src/Laison.MES.Client/Program.cs
+++ src/Laison.MES.Client/Program.cs
@@ -1,11 +1,21 @@
-using Microsoft.AspNetCore.Components.Web;
+using AntDesign.ProLayout;
 using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
-using Laison.MES.Client;
+using Microsoft.Extensions.DependencyInjection;
 
-var builder = WebAssemblyHostBuilder.CreateDefault(args);
-builder.RootComponents.Add<App>("#app");
-builder.RootComponents.Add<HeadOutlet>("head::after");
+namespace Laison.MES.Client
+{
+    public class Program
+    {
+        public static async Task Main(string[] args)
+        {
+            var builder = WebAssemblyHostBuilder.CreateDefault(args);
+            builder.RootComponents.Add<App>("#app");
 
-builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri(builder.HostEnvironment.BaseAddress) });
+            builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri(builder.HostEnvironment.BaseAddress) });
+            builder.Services.AddAntDesign();
+            builder.Services.Configure<ProSettings>(builder.Configuration.GetSection("ProSettings"));
 
-await builder.Build().RunAsync();
+            await builder.Build().RunAsync();
+        }
+    }
+}
